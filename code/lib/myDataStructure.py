f88e4e411d1a96e16f0f1b15906dfa18beddc523
shawn_jia <76928888+shawn-jia-CN@users.noreply.github.com>
2025-09-06 15:39:42 +0800

    [KIS]Add merge sort and qsort (#4)

--- code/lib/myDataStructure.py
+++ code/lib/myDataStructure.py
@@ -7,24 +7,25 @@ CAPACITY = 100
 
 
 class myList:
-    def __init__(self, cap = CAPACITY, strIn = ""):
+    def __init__(self, cap = CAPACITY, strIn = "", sortTool = None):
         self.__cap__ = cap
-        self.__len__ = 0
+        self.len = 0
         self.arr = [0] * self.__cap__
+        self.sortTool = sortTool
         if strIn != "":
             self.initWithStr(strIn)
 
 
     def __len__(self):
-        return self.__len__
+        return self.len
     
     def __getitem__(self, index):
-        if index > self.__len__ or index < 0:
+        if index > self.len or index < 0:
             return self.errorMsg(myErrorType.InvaildParameter)
         return self.arr[index]
 
     def __setitem__(self,index, value):
-        if index > self.__len__ or index < 0:
+        if index > self.len or index < 0:
             return self.errorMsg(myErrorType.InvaildParameter)
         self.arr[index] = value
 
@@ -35,13 +36,13 @@ class myList:
         if strIn != "":
             tmp = str(strIn).split(",")
             for elem in tmp:
-                self.arr[self.__len__ ] = int(elem)
-                self.__len__ = self.__len__ + 1
+                self.arr[self.len ] = int(elem)
+                self.len = self.len + 1
         return
 
 
     def getLength(self):
-        return self.__len__
+        return self.len
     
     def getCapacity(self):
         return self.__cap__
@@ -54,56 +55,56 @@ class myList:
         return ans
 
     def isFull(self):
-        return self.__len__ == self.__cap__
+        return self.len == self.__cap__
     
     def isEmpty(self):
-        return (self.__len__ == 0)
+        return (self.len == 0)
 
     def extend(self, cap):
         if cap <= self.__cap__:
             return self.errorMsg(myErrorType.InvaildParameter)
         newarr = [0] * cap
-        for i in range(self.__len__):
+        for i in range(self.len):
             newarr[i] = self.arr[i]
         del self.arr
         self.arr = newarr
         self.__cap__ = cap
 
     def insert(self, index, value):
-        if index > self.__len__ or index < 0:
+        if index > self.len or index < 0:
             return self.errorMsg(myErrorType.InvaildParameter)
         if self.isFull():
             return self.errorMsg(myErrorType.NoCapacity)
 
-        tail = self.__len__ + 1
+        tail = self.len + 1
         while tail > index:
             self.arr[tail] = self.arr[tail - 1]
             tail = tail - 1
         self.arr[index] = value
-        self.__len__ = self.__len__ + 1
+        self.len = self.len + 1
         return self.errorMsg(myErrorType.NoError)
     
     def remove(self, index):
-        if index > self.__len__ or index < 0:
+        if index > self.len or index < 0:
             return self.errorMsg(myErrorType.InvaildParameter)
         if self.isEmpty():
             return self.errorMsg(myErrorType.InvaildParameter)
         
         head = index
-        while index < self.__len__:
+        while index < self.len:
             self.arr[index] = self.arr[index + 1]
             index = index + 1
-        self.arr[self.__len__] = 0
-        self.__len__ = self.__len__ -1 
+        self.arr[self.len] = 0
+        self.len = self.len -1 
 
         return 
 
     def append(self, value):
-        self.insert(self.__len__, value)
+        self.insert(self.len, value)
         return 
     
     def pop(self):
-        self.remove(self.__len__ - 1)
+        self.remove(self.len - 1)
         return
     
     def printMyList(self):
@@ -112,4 +113,7 @@ class myList:
     def errorMsg(self, code):
         return myErrorType.myRerunValue(returnCode=code)
         
+    
+    def doSort(self):
+        self.sortTool.sort(self)
 
